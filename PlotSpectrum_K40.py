3f8948a58a43a9923e03d86f108783f86c01564a
Neutrinos Angra <angra@localhost.localdomain>
2019-08-30 17:21:28 -0300

    Linear fit full range

--- PlotSpectrum_K40.py
+++ PlotSpectrum_K40.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import optimize
 
 # Load data
 f = open('AmostraK40_2019.txt')
@@ -24,8 +25,24 @@ for line in f:
 colors = ( 0 , 0 , 0 )
 area   = np.pi*3
 
+def lin_func(x, a, b):
+    return a * x + b
+
+v_lin_func = np.vectorize(lin_func)
+
+params, params_cov = optimize.curve_fit(lin_func, x, y)
+
+print(params) 
+
+y_fit = v_lin_func(x, params[0], params[1])
+print(y_fit)
+
 # Plot
 plt.scatter( x, y, s=area, c=colors, alpha=0.5)
+plt.scatter(x,y_fit)
 plt.xlabel('E[KeV]')
 plt.ylabel('Counts')
-plt.savefig("PlotSpectrum_K40.jpg")
+#plt.axis([930,970,10,11])
+#plt.savefig("PlotSpectrum_K40.jpg")
+plt.show()
+
